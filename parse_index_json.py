ce0b49788f30dc712fa930250e29c4a900c3cbb1
Sumedha Pramod <sumedha.pramod@gmail.com>
2020-03-12 21:49:05 -0700

    Scrape tacofancy API

---
+++ parse_index_json.py
@@ -0,0 +1,18 @@
+import json
+
+
+def scrape_index_into_files(category):
+    with open('{}/index.json'.format(category)) as f:
+        indexData = json.load(f)
+
+    for item in indexData:
+        with open("{}/{}.json".format(category, item['slug']), "w") as item_file:
+            json.dump(item, item_file)
+        item_file.close()
+
+    f.close()
+
+
+directories = ["toppings", "base_layers", "mixins", "seasonings", "shells"]
+for directory in directories:
+    scrape_index_into_files(directory)

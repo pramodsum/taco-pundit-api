35b267eed06f25077f32c0faf3f4ed8eec226eb0
Sumedha Pramod <sumedha.pramod@gmail.com>
2020-03-12 22:26:08 -0700

    fixing script

--- parse_index_json.py
+++ parse_index_json.py
@@ -1,20 +1,24 @@
 import json
 import uuid
 
-
-def scrape_index_into_files(category):
-    with open('{}/index.json'.format(category)) as f:
-        indexData = json.load(f)
-
-    for item in indexData:
-        with open("{}/{}.json".format(category, item['slug']), "w") as item_file:
-            item['uuid'] = "{}".format(uuid.uuid1())
-            json.dump(item, item_file)
-        item_file.close()
-
-    f.close()
-
-
 directories = ["toppings", "base_layers", "mixins", "seasonings", "shells"]
+
 for directory in directories:
-    scrape_index_into_files(directory)
+    updatedIndexData = []
+
+    with open('{}/index.json'.format(directory)) as readable_file:
+        # print(readable_file)
+        indexData = json.load(readable_file)
+
+        for item in indexData:
+            with open("{}/{}.json".format(directory, item['slug']), "w") as item_file:
+                item['uuid'] = "{}".format(uuid.uuid1())
+                json.dump(item, item_file)
+                updatedIndexData.append(item)
+            item_file.close()
+
+        readable_file.close()
+
+    with open('{}/index.json'.format(directory), "w") as writeable_file:
+        json.dump(updatedIndexData, writeable_file)
+        writeable_file.close()
